6f3ae6813cc23ca8f1e346ab4c8ccc68f77b6c7f
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2019-07-18 21:55:57 +0200

    report builder component created

---
+++ src/app/report-builder.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-builder',
+  templateUrl: './report-builder.component.html',
+  styleUrls: ['./report-builder.component.scss']
+})
+export class ReportBuilderComponent {
+}
