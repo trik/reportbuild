30b95a1ed8a6a3ca1cfb58e9aaba4ef0483d4738
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2019-07-19 12:24:20 +0200

    report widget created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ReportBuilderComponent } from './report-builder/report-builder.component';
+import { ReportComponent } from './widgets/report/report.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReportBuilderComponent
+    ReportBuilderComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/widgets/widget-type.js
@@ -0,0 +1,12 @@
+export const WidgetType = {
+	Layout: 0,
+  PageBreak: 1,
+  Image: 2,
+  Text: 3,
+  Chart: 4,
+  Table: 5,
+  Map: 6,
+  Column: 7,
+  Formula: 8,
+  ImageContainer: 9
+};
