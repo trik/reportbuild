8d1b83da9bffc3f13196fd52b9a2ae9535424650
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2019-07-23 17:13:47 +0200

    dynamic creation of layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ReportBuilderComponent } from './report-builder/report-builder.component';
 import { ReportComponent } from './widgets/report/report.component';
+import { LayoutComponent } from './widgets/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReportBuilderComponent,
-    ReportComponent
+    ReportComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
+  entryComponents: [ReportComponent, LayoutComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/widgets/report/report.component.ts
+++ src/app/widgets/report/report.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ComponentFactory,
+  ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
+
+import { LayoutComponent } from '../layout/layout.component';
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.scss']
 })
-export class ReportComponent implements OnInit {
+export class ReportComponent implements AfterViewInit {
+  private layoutFactory: ComponentFactory<LayoutComponent>;
 
-  constructor() { }
+  @ViewChild('content', {read: ViewContainerRef})
+  content: ViewContainerRef;
 
-  ngOnInit() {
+  constructor(private factoryResolver: ComponentFactoryResolver) {
+  	this.layoutFactory = this.factoryResolver.resolveComponentFactory(LayoutComponent);
+  }
+
+  ngAfterViewInit() {
+  	this.content.createComponent(this.layoutFactory);
   }
 
 }
