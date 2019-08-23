6028cf6868b66aa96ea35e5564b345009a6a8193
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2019-08-23 14:45:06 +0200

    load json from file implemented

--- src/app/report-builder/report-builder.component.ts
+++ src/app/report-builder/report-builder.component.ts
@@ -16,4 +16,24 @@ export class ReportBuilderComponent {
   report: Report = {header: {content: []}, content: {content: []}, footer: {content: []}};
 
   constructor() { }
+
+  onLoadJson(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    if (!file) {
+      return;
+    }
+    const reader = new FileReader();
+    reader.onload = ev => {
+      const text = (ev.target as any).result;
+      let report: any;
+      try {
+        report = JSON.parse(text);
+      } catch (ex) {
+        window.alert('Invalid json: ' + ex);
+        return;
+      }
+      this.report = report;
+    };
+    reader.readAsText(file);
+  }
 }
