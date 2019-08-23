356dfb81fb7985a2e6e0f8a8473d47a0ae200aca
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2019-08-23 12:02:26 +0200

    table: readonly support for rowspan/colspan

--- src/app/widgets/table/table.component.ts
+++ src/app/widgets/table/table.component.ts
@@ -20,6 +20,22 @@ export class TableComponent extends WidgetComponent {
     super(builder);
   }
 
+  numColumns(): number {
+    let n = 0;
+    for (const cell of this.table.dataset[0]) {
+      n += cell.colspan || 1;
+    }
+    return n;
+  }
+
+  numRows(): number {
+    let n = 0;
+    for (const row of this.table.dataset) {
+      n += row[0].rowspan || 1;
+    }
+    return n;
+  }
+
   addRow() {
     const dataset = this.table.dataset;
     dataset.push(dataset[0].map(_ => emptyTableCell()));
