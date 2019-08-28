077f5cd511da4e0da358b5754473aab5dca58632
trik <marco.marche@gnucoop.com>
2019-08-28 17:41:21 +0200

    build: circleci config, first try

--- karma.conf.js
+++ karma.conf.js
@@ -26,6 +26,24 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadless:  {
+        base: 'Chrome',
+        flags:  [
+          '--no-sandbox',
+          '--allow-insecure-localhost',
+          '--homepage=about:blank',
+          '--no-first-run',
+          '--headless',
+          '--disable-software-rasterizer',
+          '--disable-gpu',
+          '--disable-translate',
+          '--disable-extensions',
+          '--disable-setuid-sandbox',
+          '--remote-debugging-port=9876'
+        ],
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
